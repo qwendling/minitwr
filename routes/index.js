84e2389c3c78b2dc16a345e002015d04ff22e25b
MBrigel <maxime.brigel@etu.unistra.fr>
2015-05-12 14:10:05 +0200

    Modification de l'index et ajout de pages

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var tweets = [];
+var messages = [];
+var currentdate = new Date();
+var date = currentdate.getDate()+ "/" + (currentdate.getMonth()+1);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', tweets: tweets });
 });
+
+router.get('/messages', function(req, res, next) {
+  res.render('messages', { title: 'Express', messages: messages });
+});
+
+router.get('/contacts', function(req, res, next) {
+  res.render('contacts', { title: 'Express', tweets: tweets });
+});
+
 router.post('/', function(req, res, next) {
   tweets.unshift(req.body.tweet);
   res.redirect('/')
 });
+
+router.post('/messages', function(req, res, next) {
+  messages.unshift(req.body.mess);
+  res.redirect('messages')
+});
+
 module.exports = router;
